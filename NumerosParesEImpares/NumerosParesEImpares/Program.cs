5983ad23daab9cb2855a5875f96ad304607b8cb8
Joel Lopez <jallende153@gmail.com>
2017-10-06 13:15:26 -0500

    primera versión para obtener si el número es par o impar

--- NumerosParesEImpares/NumerosParesEImpares/Program.cs
+++ NumerosParesEImpares/NumerosParesEImpares/Program.cs
@@ -10,6 +10,28 @@ namespace NumerosParesEImpares
     {
         static void Main(string[] args)
         {
+
+            //===============================================
+            // declaracion de varibles 
+            int n;
+            int e;
+            double d;
+            //===============================================
+            
+            //===============================================
+            // Ingresar el número y guardarlo en la variable n, 
+            //realizar los cálculos para averiguar si el número ingresado es par o impar 
+            Console.Write("Ingrese un número: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            e = n / 2;
+            d = (double)n / 2;
+            if (d - e == 0)
+                // muestra el mensaje en pantalla
+                Console.WriteLine("El Número " + n + " es par");
+            else
+                Console.WriteLine("El Número " + n + " es impar");
+            Console.ReadKey();
+            //===============================================
         }
     }
 }
